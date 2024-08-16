9df690e2f3c816e23ceb9167f6b53f4cfc0938ce
jordanrafaell <jordanrafaelflorencio@gmail.com>
2024-08-15 21:12:47 -0300

    Refactor: enhance banner with additional button and styles

--- frontend/src/app/pages/banner.js
+++ frontend/src/app/pages/banner.js
@@ -12,11 +12,16 @@ const Banner = () => (
         <h1>Connect to the Future of Integrations</h1>
         <h2>Powerful APIs for Smart Business Solutions</h2>
         <p>Discover how our APIs can transform your processes, streamlining integrations and automations to boost your business efficiency.</p>
-        <button class="btn_explore">
-          <span>
-            Explore Api
-          </span>
-        </button>
+        <div className="container_buttons">
+          <button class="btn_explore">
+            <span>
+              Explore Api
+            </span>
+          </button>
+          <button className='btn_started'>
+            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Get Started
+          </button>
+        </div>
       </div>
       <div className="container_img">
         <img src="/images/banners/banner.png" alt="Imagem ilustrativa" />
