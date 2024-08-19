d2fb747ae5ec9a272b0d423160923b00480b3e24
jordanrafaell <jordanrafaelflorencio@gmail.com>
2024-08-19 07:35:17 -0300

    Organize projects structure: Styles, Libs, and imports

---
+++ frontend/src/app/utils/import-css/style.js
@@ -0,0 +1,6 @@
+// Styles.js
+import '../../styles/banner.css';
+import '../../styles/globals.css';
+import '../../styles/header.css';
+import '../../styles/media.css'
+import '../../styles/about.css';
---
+++ frontend/src/app/utils/libs/libs.js
@@ -0,0 +1,13 @@
+// libs.js
+import { motion } from 'framer-motion'
+import Script from 'next/script';
+import {React} from 'react';
+
+
+const libs = {
+  motion,
+  Script,
+  React,
+}
+
+export default libs
