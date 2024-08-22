1a042a6fac232be9e94a2ea160ad495f512c02e6
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-08-21 23:31:21 -0300

    Refactor banner component and styles

--- frontend/src/app/pages/banner.js
+++ frontend/src/app/pages/banner.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import Script from 'next/script';
-import '../styles/banner.css';
-import '../styles/globals.css';
-import '../styles/mediaQuery.css';
+'use client'
+import libs from '../utils/libs/libs.js'
+import '../utils/import-css/style.js'
+const { motion, Script, React } = libs
 
 const Banner = () => (
   <section className='Banner'>
-    <div className="img_background"></div>
     <div className="container_banner">
-      <div className="container_text">
+      <motion.div className="container_text"
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 1, x: -100 }}
+        transition={{ duration: 3 }}
+      >
         <h1>Connect to the Future of Integrations</h1>
         <h2>Powerful APIs for Smart Business Solutions</h2>
         <p>Discover how our APIs can transform your processes, streamlining integrations and automations to boost your business efficiency.</p>
         <div className="container_buttons">
-          <button class="btn_explore">
+          <button className="btn_explore">
             <span>
               Explore Api
             </span>
           </button>
           <button className='btn_started'>
-            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Get Started
+            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin='round' className="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Get Started
           </button>
         </div>
-      </div>
+      </motion.div>
       <div className="container_img">
         <img src="/images/banners/banner.png" alt="Imagem ilustrativa" />
       </div>
