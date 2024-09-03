58b0480938c502a5e9b22d279a1a6ec25ab696d6
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-09-03 07:37:26 -0300

    Corrigido problemas de build e reinstaladas dependências.3

--- frontend/src/app/utils/libs/libs.js
+++ frontend/src/app/utils/libs/libs.js
@@ -3,10 +3,10 @@ import { motion } from 'framer-motion';
 import Script from 'next/script';
 import Link from 'next/link';
 import React, { PureComponent } from 'react';
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from 'recharts';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faGoogle, faFacebookF, faLinkedin  } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from 'recharts'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGoogle, faFacebookF, faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons'; 
 
 const libs = {
   motion,
