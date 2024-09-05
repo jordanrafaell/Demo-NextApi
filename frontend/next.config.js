835451a122e0fc69814ab19dced04a1908360ec2
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-09-05 19:02:41 -0300

    passando a pasta app para raiz do projeto

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true, // Habilita a estrutura de diretórios `app`
   },
-  // output: 'export', // exportando como HTML estático
+  output: 'export', // exportando como HTML estático
   distDir: 'out', // Gera o build em uma pasta chamada 'out'
-  srcDir: '/frontend/src', // especificando que as pastas app/pages estão dentro de src
 };
 
 module.exports = nextConfig;
