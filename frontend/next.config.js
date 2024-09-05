4c0dd76649ff5696787e2abc8d4309d3547d3b7a
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-09-05 18:54:45 -0300

    configuração de pasta

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true, // Habilita a nova estrutura de diretórios `app`
+    appDir: true, // Habilita a estrutura de diretórios `app`
   },
-  output: 'export',
-  distDir: 'out',
-  basePath: '/out'
+  // output: 'export', // exportando como HTML estático
+  distDir: 'out', // Gera o build em uma pasta chamada 'out'
+  srcDir: '/frontend/src', // especificando que as pastas app/pages estão dentro de src
 };
 
 module.exports = nextConfig;
